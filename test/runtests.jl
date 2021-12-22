bcd85dba7596dde3faa9420093ac11afd2a32df6
chengchingwen <adgjl5645@hotmail.com>
2021-12-23 00:29:38 +0800

    simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,23 @@ using StructWalk
 using Test
 
 @testset "StructWalk.jl" begin
-    # Write your tests here.
+    @test postwalk(x->x isa Expr ? eval(x) : x, :(2*3+5)) == 2*3+5
+    @test postwalk(x->x isa Expr ? eval(x) : x==:+ ? :* : x==:* ? :+ : x, :(2*3+5)) == (2+3)*5
+
+    struct C
+        x::Int
+        y::Int
+        z::Int
+    end
+    x = (a=3, b=5, c=(x=4, y=4, z=6), d=((((5,),),),))
+    @test postwalk(x) do x
+        x isa NamedTuple{(:x, :y, :z)} && return C(x...)
+        x isa Tuple && return first(x)
+        x isa Integer && return float(x)
+        return x
+    end == (a=3.0, b=5.0, c=C(4., 4., 6.), d=5.)
+
+    f(x) = x isa Integer ? StructWalk.LeafNode(x // 2) : x isa Tuple ? =>(x .+ 1...) : x
+    @test prewalk(f, (3, 5)) == (2//1 => 3//1)
+
 end
