cf5bf728308f8b3bfeb73e10d695c23ddaa69c12
chengchingwen <adgjl5645@hotmail.com>
2022-02-13 17:26:02 +0800

    use abstract type for array and dict

--- src/walkstyle.jl
+++ src/walkstyle.jl
@@ -1,5 +1,5 @@
-walkstyle(::Type{WalkStyle}, x::T) where {T <: Array} = t->convert(AbstractArray, t), (x,), true
+walkstyle(::Type{WalkStyle}, x::T) where {T <: AbstractArray} = t->convert(AbstractArray, t), (x,), true
 walkstyle(::Type{WalkStyle}, x::T) where {T <: Tuple} = Tuple, (x,), true
 walkstyle(::Type{WalkStyle}, x::T) where {T <: NamedTuple} = let name=keys(x); x->NamedTuple{name}(x); end, (x,), true
 walkstyle(::Type{WalkStyle}, x::Expr) = (head, args)->Expr(head, args...), (x.head, x.args)
-walkstyle(::Type{WalkStyle}, x::T) where {T <: Dict} = Dict, ((p for p in x),), true
+walkstyle(::Type{WalkStyle}, x::T) where {T <: AbstractDict} = Dict, ((p for p in x),), true
