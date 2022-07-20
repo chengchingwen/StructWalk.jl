3f00b8fe7b7cc71dacb055d44b1efb772b11729e
chengchingwen <adgjl5645@hotmail.com>
2022-07-20 15:52:07 +0800

    default aligned style

--- src/aligned.jl
+++ src/aligned.jl
@@ -1,12 +1,18 @@
 abstract type AlignedStyle{W<:WalkStyle} end
 
-const ALIGNED = Union{AlignedStyle, Type{AlignedStyle}}
+struct DefaultAlignedStyle{W} <: AlignedStyle{W}
+    walkstyle::W
+end
 
-WalkStyle(::AlignedStyle{W}) where W = W
+const ALIGNED = Union{AlignedStyle, Type{<:AlignedStyle}, DefaultAlignedStyle}
+
+WalkStyle(style::DefaultAlignedStyle) = style.walkstyle
+WalkStyle(::AlignedStyle{W}) where W = W()
 WalkStyle(::Type{AlignedStyle}) = WalkStyle
+WalkStyle(::Type{<:AlignedStyle{W}}) where W = W()
 
 constructor(s::ALIGNED, x::T, y::T, z::T...) where T = T
-constructor(s::ALIGNED, x::T, y::T, z::T...) where {name, T <: NamedTuple{name}} = NamedTuple{name}
+constructor(s::ALIGNED, x::NamedTuple{name}, y::NamedTuple{name}, z::NamedTuple{name}...) where name = NamedTuple{name}
 constructor(s::ALIGNED, x::Union{NamedTuple, Tuple}, y::Union{NamedTuple, Tuple}, z::Union{NamedTuple, Tuple}...) = Tuple
 constructor(s::ALIGNED, x, y, z...) = Vector
 
@@ -38,13 +44,17 @@ function walk(f, g, style::ALIGNED, inner_walk, x, y, z...)
 end
 
 postwalk(f, x, y, z...) = postwalk(f, AlignedStyle, x, y, z...)
+postwalk(f, style::WalkStyle, x, y, z...) = postwalk(f, DefaultAlignedStyle(style), x, y, z...)
 postwalk(f, style::ALIGNED, x, y, z...) = walk(f, style, x -> postwalk(f, style, x...), x, y, z...)
 
 prewalk(f, x, y, z...) = prewalk(f, AlignedStyle, x, y, z...)
+prewalk(f, style::WalkStyle, x, y, z...) = prewalk(f, DefaultAlignedStyle(style), x, y, z...)
 prewalk(f, style::ALIGNED, x) = walk(identity, style, x -> prewalk(f, style, x...), f(x), f(y), map(f, z)...)
 
 mapleaves(f, x, y, z...) = mapleaves(f, AlignedStyle, x, y, z...)
+mapleaves(f, style::WalkStyle, x, y, z...) = mapleaves(f, DefaultAlignedStyle(style), x, y, z...)
 mapleaves(f, style::ALIGNED, x, y, z...) = walk(f, identity, style, x -> mapleaves(f, style, x...), x, y, z...)
 
 mapnonleaves(f, x, y, z...) = mapnonleaves(f, AlignedStyle, x, y, z...)
+mapnonleaves(f, style::WalkStyle, x, y, z...) = mapnonleaves(f, DefaultAlignedStyle(style), x, y, z...)
 mapnonleaves(f, style::ALIGNED, x, y, z...) = walk(identity, f, style, x -> mapnonleaves(f, style, x...), x, y, z...)
